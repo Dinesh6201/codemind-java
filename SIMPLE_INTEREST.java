eda9573c9dfc7dd82be4a63081865382aee363ac
Dinesh Yandrapu <112755198+Dinesh6201@users.noreply.github.com>
2022-09-11 23:28:49 +0530

    import java.util.Scanner; class hgf {     public static void main(String args[])     {         Scanner sc=new Scanner(System.in);         int p,t,r,i;         p = sc.nextInt();         t = sc.nextInt();         r = sc.nextInt();         i = p*r*t/100;      System.out.println(i);     } }

---
+++ SIMPLE_INTEREST.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+class hgf
+{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        int p,t,r,i;
+        p = sc.nextInt();
+        t = sc.nextInt();
+        r = sc.nextInt();
+        i = p*r*t/100;
+     System.out.println(i);
+    }
+}
